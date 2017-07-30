00b859988a0b3ffeb15c0ad486f57e28bb6b1bb5
ergonwork <ergonwork@gmail.com>
2017-07-30 16:52:13 +0530

    dynamic settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -91,6 +91,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#          'NAME': 'ecommerce',                      # Or path to database file if using sqlite3.
+#          'USER': 'ecommerce',                     # Not used with sqlite3.
+#          'PASSWORD': 'ecommerce',                  # Not used with sqlite3.
+#          'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#          'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#      },
+# }
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
